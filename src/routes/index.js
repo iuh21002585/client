9e1f04a27d3d68393a6996823a8e2278a2e29920
Hai <buiduchai276@gmail.com>
2024-05-27 18:46:27 +0700

    add /

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,11 @@ import V1Router from "./v1.js";
 function route(app) {
     app.use("/api/auth", AuthRouter);
     app.use("/api/v1", V1Router);
+    app.use("/", (req, res) => {
+        res.json({
+            message: "Welcome to Coffee API"
+        });
+    });
 }
 
 export default route;
